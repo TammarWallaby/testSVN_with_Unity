c98d079b250001bb9a5445d1c36f7a25b49da18c
TammarWallaby <lek8605@gmail.com>
2024-07-30 16:52:53 +0900

    SVN 수정

--- Assets/testScript.cs
+++ Assets/testScript.cs
@@ -7,7 +7,7 @@ public class testScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello SVN");
     }
 
     // Update is called once per frame
